85b7e0a59138a1309f30f7c340fe9fc2cc9bc1f6
k3eenz <osipenkomd@mer.ci.nsu.ru>
2024-11-25 09:04:54 +0700

    Update Program.cs

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Decorator;
 
-INotifier notifier = new EmailNotify("123@gmail.com");
-
-notifier = new SMSDecorator(notifier, "+1234567890");
-
-notifier = new FacebookDecorator(notifier, "kenz");
-
-notifier.Send("Hello!!!");
+INotifier notifier = new Decorator.EmailNotify("admin@example.com");
+notifier = new Decorator.SMSDecorator(notifier, "+1234567890");
+notifier = new Decorator.FacebookDecorator(notifier, "admin_facebook");
+notifier.Send("Сервер упал!");
